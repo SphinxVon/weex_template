228967b5feb7cd37da4ffafa7935bb615290bcdd
varSmallRookie <923665120@qq.com>
2018-04-09 17:20:46 +0800

    project init

--- meta.js
+++ meta.js
@@ -1,35 +1,35 @@
 const path = require('path')
 const fs = require('fs')
 
-// const {
-//   sortDependencies,
-//   installDependencies,
-//   runLintFix,
-//   printMessage,
-// } = require('./utils')
-// const pkg = require('./package.json')
+const {
+  sortDependencies,
+  installDependencies,
+  runLintFix,
+  printMessage,
+} = require('./utils')
+const pkg = require('./package.json')
 
-// const templateVersion = pkg.version
+const templateVersion = pkg.version
 
-// const { addTestAnswers } = require('./scenarios')
+const { addTestAnswers } = require('./scenarios')
 
 module.exports = {
   metalsmith: {
     // When running tests for the template, this adds answers for the selected scenario
-    // before: addTestAnswers
+    before: addTestAnswers
   },
   helpers: {
-    // if_or(v1, v2, options) {
+    if_or(v1, v2, options) {
 
-    //   if (v1 || v2) {
-    //     return options.fn(this)
-    //   }
+      if (v1 || v2) {
+        return options.fn(this)
+      }
 
-    //   return options.inverse(this)
-    // },
-    // template_version() {
-    //   return templateVersion
-    // },
+      return options.inverse(this)
+    },
+    template_version() {
+      return templateVersion
+    },
   },
   
   prompts: {
@@ -44,7 +44,7 @@ module.exports = {
       type: 'string',
       required: false,
       message: 'Project description',
-      default: 'A Vue.js project',
+      default: 'A weex project',
     },
     author: {
       when: 'isNotTest',
@@ -158,9 +158,39 @@ module.exports = {
     // },
   },
   filters: {
-    
+    // '.eslintrc.js': 'lint',
+    // '.eslintignore': 'lint',
+    // 'config/test.env.js': 'unit || e2e',
+    // 'build/webpack.test.conf.js': "unit && runner === 'karma'",
+    // 'test/unit/**/*': 'unit',
+    // 'test/unit/index.js': "unit && runner === 'karma'",
+    // 'test/unit/jest.conf.js': "unit && runner === 'jest'",
+    // 'test/unit/karma.conf.js': "unit && runner === 'karma'",
+    // 'test/unit/specs/index.js': "unit && runner === 'karma'",
+    // 'test/unit/setup.js': "unit && runner === 'jest'",
+    // 'test/e2e/**/*': 'e2e',
+    // 'src/router/**/*': 'router',
   },
   complete: function(data, { chalk }) {
-    // printMessage('下载wan', chalk)
+    // const green = chalk.green
+
+    // sortDependencies(data, green)
+
+    // const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    // if (data.autoInstall) {
+    //   installDependencies(cwd, data.autoInstall, green)
+    //     .then(() => {
+    //       return runLintFix(cwd, data, green)
+    //     })
+    //     .then(() => {
+    //       printMessage(data, green)
+    //     })
+    //     .catch(e => {
+    //       console.log(chalk.red('Error:'), e)
+    //     })
+    // } else {
+    //   printMessage(data, chalk)
+    // }
   },
 }
